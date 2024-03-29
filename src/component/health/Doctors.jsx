4d3c88a52b96d1da7422837f7433a60da9d1f967
Dollymm <100014305+Dollymm@users.noreply.github.com>
2024-03-29 16:59:33 +0530

    new commit

--- src/component/health/Doctors.jsx
+++ src/component/health/Doctors.jsx
@@ -1,28 +1,39 @@
-import React from 'react';
-import { doctor } from '../../../data';
+import React, { useEffect, useState } from 'react';
+
+import axios from 'axios';
 
 const Doctors = () => {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const doctorRes = await axios.get('https://doctor-backend-6.onrender.com/doctors');
+        setData(doctorRes.data); 
+        console.log(doctorRes.data);
+      } catch (error) {
+        console.error("Error fetching data:", error);
+      }
+    };
+
+    fetchData();
+  }, []);
+
   return (
     <div className="w-full md:w-3/5 mx-auto px-4 mb-4">
       <h2 className="text-2xl font-bold mb-4">Popular Doctors Available</h2>
-      {doctor.map((dr) => (
-        <div key={dr.id} className="bg-blue-900 rounded-lg shadow-md p-4 mb-4 text-white ">
+      {data.map((dr,id) => (
+        <div key={id} className="bg-blue-900 rounded-lg shadow-md p-4 mb-4 text-white ">
           <div className="flex flex-wrap md:flex-nowrap items-center">
             <div className="md:w-1/4 md:mr-4 md:text-center mb-4 md:mb-0">
-            
-                <img src={dr.img} className="w-24 h-24 rounded-full mx-auto mb-4 md:mb-0" alt="Doctor Image" />
-              
+              <img src='' className="w-24 h-24 rounded-full mx-auto mb-4 md:mb-0" alt="Doctor Image" />
               <div className="text-center text-white font-bold">View Profile</div>
             </div>
             <div className="md:w-3/4 ml-4">
-             
-                <h3 className="text-xl font-semibold mb-2">{dr.name}</h3>
-              
+              <h3 className="text-xl font-semibold mb-2">{dr.name}</h3>
               <div className="text-sm mb-2">{dr.experience}</div>
-              <div className="text-sm mb-2">{dr.specilazation}</div>
+              <div className="text-sm mb-2">{dr.spec}</div>
               <div className="text-sm mb-2">{dr.hospital}</div>
-           
-              
             </div>
           </div>
           <hr className="my-4 border-t border-gray-900 " />
@@ -31,18 +42,16 @@ const Doctors = () => {
               <h3 className="font-semibold mb-1">Consultation Fee</h3>
               <div className="font-bold">₹ 299/-</div>
             </div>
-         
-              <button className="button-2 font-semibold border boder-indigo-500 bg-white hover:bg-blue-900 hover:text-white  text-black px-4 py-2 rounded-lg  transition duration-300">Consult Now</button>
-            
+            <button className="button-2 font-semibold border boder-indigo-500 bg-white hover:bg-blue-900 hover:text-white text-black px-4 py-2 rounded-lg transition duration-300">Consult Now</button>
           </div>
         </div>
       ))}
       <div className="flex justify-center">
-  <button className="border border-indigo-800 hover:bg-blue-900 hover:text-white text-black rounded-lg px-2 py-2 font-semibold transition duration-300 w-48">View more</button>
-</div>
-
+        <button className="border border-indigo-800 hover:bg-blue-900 hover:text-white text-black rounded-lg px-2 py-2 font-semibold transition duration-300 w-48">View more</button>
+      </div>
     </div>
   );
 };
+
 
 export default Doctors;
--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,106 +1,194 @@
-import  { useState } from 'react';
-import Card from '@mui/material/Card';
-import {TimePicker} from 'antd'
-
+import { useState } from "react";
+import Card from "@mui/material/Card";
+import { TimePicker } from "antd";
+import axios from "axios";
 
 const Register = () => {
-    const [doctorInfo, setDoctorInfo] = useState({
-        name: '',
-        email: '',
-        specialty: '',
-        hospital: '',
-        experience:'',
-        address: '',
-        phoneNumber: '',
-        timing:{
-            start:"",
-            end:""
-        },
-    });
+  const [doctorInfo, setDoctorInfo] = useState({
+    name: "",
+    email: "",
+    specialty: "",
+    hospital: "",
+    experience: "",
+    address: "",
+    phoneNumber: "",
+    timing: {
+      start: "",
+      end: "",
+    },
+  });
 
-    const handleChange = (e) => {
-        const { name, value } = e.target;
-        setDoctorInfo((prevDoctorInfo) => ({ ...prevDoctorInfo, [name]: value }));
-    };
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setDoctorInfo((prevDoctorInfo) => ({ ...prevDoctorInfo, [name]: value }));
+  };
 
-    const handleSubmit = (e) => {
-        e.preventDefault();
-      
-        console.log('Doctor information submitted:', doctorInfo);
-     
-        // setDoctorInfo({
-        //     name: '',
-        //     email: '',
-        //     specialty: '',
-        //     hospital: '',
-        //     experience:'',
-        //     address: '',
-        //     phoneNumber: '',
-        // });
-    };
+  const handleSubmit = async (e) => {
+    e.preventDefault();
 
-    const handleTime=(time,timeString)=>{
-        console.log("time",time)
-        console.log("time String",timeString[1])
+    console.log('Doctor information submitted:', doctorInfo);
 
-        setDoctorInfo(pre=>({
-            ...pre,
-            timing:{
-                start:timeString[0],
-                end:timeString[1]
-            }
-        }))
+    try {
+      const response = await axios.post(
+        "https://doctor-backend-6.onrender.com/register",
+        {
+          name: doctorInfo.name,
+          email:doctorInfo.email,
+          experience:doctorInfo.experience,
+          hospital:doctorInfo.hospital,
+          number:doctorInfo.phoneNumber,
+          address:doctorInfo.address,
+          timing:{
+            start:doctorInfo.timing.start,
+            end:doctorInfo.timing.end
+          }
+
+          ,
+          spec:doctorInfo.specialty,
+        }
+      );
+      console.log("resposne:", response);
+    } catch (error) {
+      console.log("Error:", error);
     }
+  };
 
-    return (
-        <div className='flex justify-center items-center mt-10 '>
-            <Card className='w-[700px] p-6 bg-white rounded-lg shadow-md'>
-                <h1 className='text-2xl font-bold mb-2 text-center'>Doctor Registration</h1>
-                <p className='text-gray-600 mb-4 text-center'>Enter your information to register as a doctor</p>
-                <form onSubmit={handleSubmit}>
-                    <div className="grid grid-cols-2 gap-4">
-                        <div>
-                            <label  className="block text-gray-700 font-bold mb-2">Name</label>
-                            <input type="text" name="name" className="border border-gray-300 rounded-md py-2 px-3 w-full" value={doctorInfo.name} onChange={handleChange} required />
-                        </div>
-                        <div>
-                            <label  className="block text-gray-700 font-bold mb-2">Email</label>
-                            <input type="email" name="email" className="border border-gray-300 rounded-md py-2 px-3 w-full" value={doctorInfo.email} onChange={handleChange} required />
-                        </div>
-                        <div>
-                            <label  className="block text-gray-700 font-bold mb-2">Specialty</label>
-                            <input type="text"  name="specialty" className="border border-gray-300 rounded-md py-2 px-3 w-full" value={doctorInfo.specialty} onChange={handleChange} required />
-                        </div>
-                        
-                        <div>
-                            <label className="block text-gray-700 font-bold mb-2">Experience</label>
-                            <input type="number" id="experience" name="experience" className="border border-gray-300 rounded-md py-2 px-3 w-full" value={doctorInfo.experience} onChange={handleChange} required />
-                        </div>
-                        
-                        <div className="col-span-2">
-                            <label  className="block text-gray-700 font-bold mb-2">Hospital</label>
-                            <input type="text"  name="hospital" className="border border-gray-300 rounded-md py-2 px-3 w-full" value={doctorInfo.hospital} onChange={handleChange} required />
-                        </div>
-                        <div className="col-span-2">
-                            <label  className="block text-gray-700 font-bold mb-2">Address</label>
-                            <input type="text"  name="address" className="border border-gray-300 rounded-md py-2 px-3 w-full" value={doctorInfo.address} onChange={handleChange} required />
-                        </div>
-                        <div>
-                            <label  className="block text-gray-700 font-bold mb-2">Phone Number</label>
-                            <input type="text" name="phoneNumber" className="border border-gray-300 rounded-md py-2 px-3 w-full" value={doctorInfo.phoneNumber} onChange={handleChange} required />
-                        </div>
-                        <div>
+  const handleTime = (time, timeString) => {
+    console.log("time", time);
+    console.log("time String", timeString[1]);
 
-                        <label  className="block text-gray-700 font-bold mb-2">Timing</label>
-                        <TimePicker.RangePicker className='border border-gray-300 rounded-md py-2 px-3 w-full ' onChange={handleTime}/>
-                        </div>
-                    </div>
-                    <button type="submit" className="bg-blue-500 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline hover:bg-blue-600 flex justify-center items-center w-1/2 mx-auto mt-4">Register</button>
-                   
-                </form>
-            </Card>
-        </div>
-    );
+    setDoctorInfo((pre) => ({
+      ...pre,
+      timing: {
+        start: timeString[0],
+        end: timeString[1],
+      },
+    }));
+  };
+
+  return (
+    <div className="flex justify-center items-center mt-10 ">
+      <Card className="w-[700px] p-6 bg-white rounded-lg shadow-md">
+        <h1 className="text-2xl font-bold mb-2 text-center">
+          Doctor Registration
+        </h1>
+        <p className="text-gray-600 mb-4 text-center">
+          Enter your information to register as a doctor
+        </p>
+        <form onSubmit={handleSubmit}>
+          <div className="grid grid-cols-2 gap-4">
+            <div>
+              <label className="block text-gray-700 font-bold mb-2">Name</label>
+              <input
+                type="text"
+                name="name"
+                className="border border-gray-300 rounded-md py-2 px-3 w-full"
+                value={doctorInfo.name}
+                onChange={handleChange}
+                required
+              />
+            </div>
+            <div>
+              <label className="block text-gray-700 font-bold mb-2">
+                Email
+              </label>
+              <input
+                type="email"
+                name="email"
+                className="border border-gray-300 rounded-md py-2 px-3 w-full"
+                value={doctorInfo.email}
+                onChange={handleChange}
+                required
+              />
+            </div>
+            <div>
+              <label className="block text-gray-700 font-bold mb-2">
+                Specialty
+              </label>
+              <input
+                type="text"
+                name="specialty"
+                className="border border-gray-300 rounded-md py-2 px-3 w-full"
+                value={doctorInfo.specialty}
+                onChange={handleChange}
+                required
+              />
+            </div>
+
+            <div>
+              <label className="block text-gray-700 font-bold mb-2">
+                Experience
+              </label>
+              <input
+                type="number"
+                id="experience"
+                name="experience"
+                className="border border-gray-300 rounded-md py-2 px-3 w-full"
+                value={doctorInfo.experience}
+                onChange={handleChange}
+                required
+              />
+            </div>
+
+            <div className="col-span-2">
+              <label className="block text-gray-700 font-bold mb-2">
+                Hospital
+              </label>
+              <input
+                type="text"
+                name="hospital"
+                className="border border-gray-300 rounded-md py-2 px-3 w-full"
+                value={doctorInfo.hospital}
+                onChange={handleChange}
+                required
+              />
+            </div>
+            <div className="col-span-2">
+              <label className="block text-gray-700 font-bold mb-2">
+                Address
+              </label>
+              <input
+                type="text"
+                name="address"
+                className="border border-gray-300 rounded-md py-2 px-3 w-full"
+                value={doctorInfo.address}
+                onChange={handleChange}
+                required
+              />
+            </div>
+            <div>
+              <label className="block text-gray-700 font-bold mb-2">
+                Phone Number
+              </label>
+              <input
+                type="text"
+                name="phoneNumber"
+                className="border border-gray-300 rounded-md py-2 px-3 w-full"
+                value={doctorInfo.phoneNumber}
+                onChange={handleChange}
+                required
+              />
+            </div>
+            <div>
+              <label className="block text-gray-700 font-bold mb-2">
+                Timing
+              </label>
+              <TimePicker.RangePicker
+                className="border border-gray-300 rounded-md py-2 px-3 w-full "
+                onChange={handleTime}
+              />
+            </div>
+          </div>
+          <button
+            type="submit"
+            className="bg-blue-500 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline hover:bg-blue-600 flex justify-center items-center w-1/2 mx-auto mt-4"
+          >
+            Register
+          </button>
+        </form>
+      </Card>
+    </div>
+  );
 };
 
 export default Register;
